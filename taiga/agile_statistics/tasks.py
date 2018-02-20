ccd9999730ef8da4b26bcbfaece77b3e5708cd06
amirgolden <amirzolotoy@gmail.com>
2018-02-21 00:23:22 +0300

    cfd function & requirements update

---
+++ taiga/agile_statistics/tasks.py
@@ -0,0 +1,23 @@
+from celery.schedules import crontab
+from celery.task import periodic_task
+import taiga.agile_statistics.stats as stats
+
+
+@periodic_task(
+    run_every=(crontab(hour=20, minute=14)),
+    name="store_daily"
+)
+def store_daily():
+    stats.main({'command': 'store_daily'})
+
+
+@periodic_task(
+    run_every=(crontab(hour=20, minute=15)),
+    name="cfd"
+)
+def cfd():
+    stats.main({'command': 'cfd'})
+
+
+def deps_dot():
+    stats.main({'command': 'deps_dot'})
